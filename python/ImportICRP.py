467edd333da20ec617ecd4bdcafa4c18fff5d0f4
apr <apr@chixstix>
2016-08-23 14:18:41 +0100

    Now makes STL files!
    Just needs cleaning up and making into a class now !
    
    Maybe split DicomToSTL into modules.

--- python/ImportICRP.py
+++ python/ImportICRP.py
@@ -12,30 +12,52 @@
 import numpy as np
 import SimpleITK as sitk
 import re
+import vtk
 
 import showData as vis
 import itkStats as ist
+import vtkClip  as vc 
 
-# rows, colums, slices
-#ConstPixelDims = (127, 254, 222)
+#----------------------------------------------
+# Define variables
+#
+# columns, rows, slices
 ConstPixelDims = (254, 127, 222)
+ConstPixelSpacing = (2.137, 2.137, 8.0)
+origin = (0,0,0)
 
-data = np.fromfile("/home/apr/Work/NPL/MRT2/Phantoms/P110 data V1.2/AM/AM.dat", sep=" ")
-data3D = data.reshape(ConstPixelDims, order='F')
+dataDir = "/home/apr/Work/NPL/MRT2/Phantoms/P110 data V1.2/"
+dataFile = "AM/AM.dat"
+outputFile = "abc"
+#----------------------------------------------
 
+#----------------------------------------------
+# Options:
+#
+innerContour   = -0.1    # Level to define volume after anti-aliasing (organs)
+#innerContour   = -0.5    # Level to define volume after anti-aliasing (cube)
+shellThickness =  2.0    # Shell thickness in voxels
+aaConverge     = 0.00005 # MaximumRMSChange for anti-aliasing (smaller = tighter but slower)
 
+relaxFactor      = 0.01 # Specify the relaxation factor for Laplacian smoothing. 
+smoothIterations = 700  # Specifiy number of smoothing iterations
 
-print data
-print data.size
+#debug = 0    # Don't show data
+#debug = 1    # Show CT and VOI
+#debug = 2    # Show final itk image
+#debug = 3    # Show both
+#showHist = 0 # Show the histogram for the anti-aliased data
+#debugVol = 0 # Show the volume for each step of calculation
+#noVis = 0 # Set variable to turn off visualing ISO su
+#----------------------------------------------
 
-print data3D
-print data3D.size
-nPixelsOrgans = np.count_nonzero(data3D)
-print nPixelsOrgans
+# Read in data
+icrpData = np.fromfile(dataDir+dataFile, sep=" ")
+icrp3D = icrpData.reshape(ConstPixelDims, order='F')
+nPixelsOrgans = np.count_nonzero(icrp3D)
+print "Read in ", nPixelsOrgans, " voxels from ", dataDir+dataFile
 
-# Read in the orgna file file
-organ_dictionary  = {} # Internally used dictionary
-
+# Read in the organ file file
 with open("/home/apr/Work/NPL/MRT2/Phantoms/P110 data V1.2/AM/AM_organs.dat", "r") as file:
 
     organID = []
@@ -61,23 +83,223 @@ print organName;
 print organTissueID
 print organDensity
         
-# Pick organ
+# Pick the organ we want to use 
 for i, val in enumerate(organName):
     if val == 'spleen':
         organ = organID[i] 
 print organ
-data3D[data3D != organ] = 0
 
-vis.arrayShow(data3D, 2)
+# Mask based on that organ
+icrp3D[icrp3D != organ] = 0
+
+# Show the raw icrp data
+vis.arrayShow(icrp3D, 2)
 
 # Create ITKimages from arrays
 print 'Creating raw itk images...'
-itkCT =sitk.GetImageFromArray(data3D)
-print '\n[itkCT Info]'
-ist.itkInfo(itkCT)
+itkOrgan =sitk.GetImageFromArray(icrp3D)
+print '\n[itkOrgan Info]'
+ist.itkInfo(itkOrgan)
 
-vis.itkShow(itkCT,2)
+# Show the itk image
+vis.itkShow(itkOrgan,2)
 
 
+################################################################################################################################
+################################
+# This taken for DicomToSTL.py #
+################################
+################################################################################################################################
 
-# TODO: Pick whcih organs you want in the image from the lookup table
+# Defoen what value corresposnds to inside the VOI
+thresholdValue = organ
+
+#---------------------------------------------------
+# Maniuplate the itk images
+print 'Manipulating itk images...'
+
+# Use this for mouse model
+#organSeeds=sitk.VectorUIntList([[172,184,ConstPixelDims[2]/2],[226,188,ConstPixelDims[2]/2]])
+#organMask=sitk.ConnectedThreshold(itkOrgan,organSeeds,15300.0,20000.0,1)
+
+# Use this for masked CT
+organMask=sitk.BinaryThreshold(itkOrgan,1,thresholdValue,1,0)
+
+organMask=sitk.BinaryClosingByReconstruction(organMask,30)
+
+#organMask=sitk.BinaryDilate(organMask)
+
+organMask=sitk.AntiAliasBinary(organMask,aaConverge) #Output from antialias filter is float64    
+
+# Show the final histogram
+vis.itkHist(organMask, "itkAA")
+
+# Show the final itk image
+vis.itkShow(organMask,2)
+#---------------------------------------------------
+
+#---------------------------------------------------
+# Read the itk image into vtk
+print "Creating vtk volume from itk image..."
+dataImporter = vtk.vtkImageImport() 
+data_string = sitk.GetArrayFromImage(organMask).tostring()
+dataImporter.CopyImportVoidPointer(data_string, len(data_string))
+dataImporter.SetDataScalarTypeToDouble()
+dataImporter.SetNumberOfScalarComponents(1)
+dataImporter.SetDataExtent(0,ConstPixelDims[2]-1,0,ConstPixelDims[1]-1,0,ConstPixelDims[0]-1)   # z y x
+dataImporter.SetWholeExtent(0,ConstPixelDims[2]-1, 0,ConstPixelDims[1]-1,0,ConstPixelDims[0]-1) # z y x
+
+dataImporter.Update()
+
+# Create VTK Image volume and set origin and spacing
+organVolume=dataImporter.GetOutput()  
+organVolume.SetOrigin(origin[1], origin[1], origin[0])                                 # z y x
+organVolume.SetSpacing(ConstPixelSpacing[2],ConstPixelSpacing[1],ConstPixelSpacing[0]) # z y x
+#---------------------------------------------------
+
+#---------------------------------------------------
+# Extract isosurface using VTK marching cubes filter
+outerContour = innerContour - shellThickness
+print "Extracting iso surfaces from ", innerContour, " to ", outerContour
+organMarch=vtk.vtkMarchingCubes()  
+organMarch.SetInputData(organVolume)
+organMarch.SetValue(0,outerContour)
+organMarch.SetValue(1,innerContour)
+organMarch.ComputeNormalsOn()
+organMarch.ComputeScalarsOn()
+organMarch.Update()
+organPolys=organMarch.GetOutput()
+
+print "Extracting inner contour = ", innerContour
+organVolMarch=vtk.vtkMarchingCubes()  
+organVolMarch.SetInputData(organVolume)
+organVolMarch.SetValue(0,innerContour) # better for organs
+organVolMarch.ComputeNormalsOn()
+organVolMarch.ComputeScalarsOn()
+organVolMarch.Update()
+organVol=organVolMarch.GetOutput()
+#--------------------------------------------------
+
+#---------------------------------------------------
+# Calculate the volume and surface area from isosurface
+massVTK = vtk.vtkMassProperties()
+massVTK.SetInputData(organVol)
+massVTK.Update() 
+
+volumeISO = massVTK.GetVolume() 
+areaISO   = massVTK.GetSurfaceArea()
+print 'volumeISO = ', volumeISO, ' areaISO = ', areaISO
+
+massVTKshell = vtk.vtkMassProperties()
+massVTKshell.SetInputData(organPolys)
+massVTKshell.Update() 
+
+volumeShell = massVTKshell.GetVolume()
+areaShell   = massVTKshell.GetSurfaceArea()
+print 'volumeShell = ', volumeShell, ' areaShell = ', areaShell
+#---------------------------------------------------
+
+#---------------------------------------------------
+# Show iso surface
+print "Rendering iso surface..."
+vis.showIso(organPolys, 'Original Shell')
+#---------------------------------------------------
+
+#--------------------------------------------------
+# Smooth the surfaces
+print "Smoothing surfaces..."
+smoothOrganPolys=vtk.vtkSmoothPolyDataFilter()
+smoothOrganPolys.SetInputData(organPolys)
+smoothOrganPolys.SetRelaxationFactor(relaxFactor)
+smoothOrganPolys.SetNumberOfIterations(smoothIterations)
+smoothOrganPolys.Update()
+organPolysSmooth=smoothOrganPolys.GetOutput()
+
+smoothOrganVol=vtk.vtkSmoothPolyDataFilter()
+smoothOrganVol.SetInputData(organVol)
+smoothOrganVol.SetRelaxationFactor(relaxFactor)
+smoothOrganVol.SetNumberOfIterations(smoothIterations)
+smoothOrganVol.Update()
+organVolSmooth=smoothOrganVol.GetOutput()
+#--------------------------------------------------
+
+#---------------------------------------------------
+# Calculate the volume and surface area from isosurface
+massVTKSmooth = vtk.vtkMassProperties()
+massVTKSmooth.SetInputData(organVolSmooth)
+massVTKSmooth.Update() 
+
+volumeISOsmooth = massVTKSmooth.GetVolume() 
+areaISOsmooth   = massVTKSmooth.GetSurfaceArea()
+print 'volumeISOsmooth = ', volumeISOsmooth, ' areaISOsmooth = ', areaISOsmooth
+
+massVTKshellSmooth = vtk.vtkMassProperties()
+massVTKshellSmooth.SetInputData(organPolysSmooth)
+massVTKshellSmooth.Update() 
+
+volumeShellsmooth = massVTKshellSmooth.GetVolume()
+areaShellsmooth   = massVTKshellSmooth.GetSurfaceArea()
+print 'volumeShellsmooth = ', volumeShellsmooth, ' areaShellsmooth = ', areaShellsmooth
+#---------------------------------------------------
+
+#---------------------------------------------------
+# Show iso surface
+print "Rendering smooth iso surface..."
+vis.showIso(organPolysSmooth, 'Smoothed Shell')
+#---------------------------------------------------
+
+#---------------------------------------------------
+# Write STL
+print "Writing STL Shell..."
+vc.writeSTL(organPolys, outputFile, '_shell.stl' )
+
+print "Writing STL Shell (smoothed)..."
+vc.writeSTL(organPolysSmooth, outputFile, '_shell_smoothed.stl' )
+
+print "Writing STL solid inner volume..."
+vc.writeSTL(organVol, outputFile, '_solid.stl' )
+
+print "Writing STL solid inner volume (smoothed)..."
+vc.writeSTL(organVolSmooth, outputFile, '_solid_smoothed.stl' )
+
+#---------------------------------------------------
+
+# #---------------------------------------------------
+# # Output a report
+# print " "
+# print "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+# print 'Processed DICOM folder: ', PathDicom
+# print ' Dimensions = ', ConstPixelDims
+# print ' Spacing    = ', ConstPixelSpacing , ' mm'
+# print ' '
+ 
+# if VOIfile:
+#     print 'Using VOI file: ', VOIfile
+#     print " "
+#     print 'Anti-alias MaximumRMSChange = ', aaConverge
+#     print 'Inner Contour   = ',  innerContour 
+#     print 'Shell Thickness = ',  shellThickness , ' voxels'
+#     print " "
+#     print 'Smoothing parameters: ', relaxFactor, ' ', smoothIterations
+#     print " "
+#     print ' {} {:15.2f} {}'.format('VOI volume            = ',volumeVOI,'mm^3')
+#     print ' {} {:15.2f} {}'.format('ISO volume            = ',volumeISO,'mm^3')
+#     print ' {} {:15.2f} {}'.format('Diff                  = ',100.0*((volumeISO - volumeVOI)/volumeVOI), '%')
+#     print ' {} {:15.2f} {}'.format('ISO volume (smoothed) = ',volumeISOsmooth,'mm^3')
+#     print ' {} {:15.2f} {}'.format('Diff                  = ',100.0*((volumeISOsmooth - volumeVOI)/volumeVOI), '%')
+#     print " "
+#     print ' {} {:15.2f} {}'.format('ISO area              = ',areaISO, 'mm^2')
+#     print ' {} {:15.2f} {}'.format('ISO area (smoothed)   = ',areaISOsmooth, 'mm^2')
+# print " "
+# print 'Output file: ' , outputFile
+# print "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+# print ' '
+# #---------------------------------------------------
+
+
+# Finished
+endTime = time.time()
+elapsed = endTime - startTime
+print 'Finished: ', elapsed, ' seconds.'
+
+
---
+++ python/vtkClip.py
@@ -0,0 +1,91 @@
+###############################################################################
+# vtkClip.py                                                                  #
+#                                                                             #
+# APR: 18/11/15                                                               #
+#                                                                             #
+# Functions to clip a vtk poly object along a plane and return the two halves #
+###############################################################################
+
+import vtk
+
+# Cut a poly object through the center of mass and return both parts
+def planeCut(polys):
+
+    # Get the enter of mass of the shell 
+    comVTK = vtk.vtkCenterOfMass()
+    comVTK.SetInputData(polys)
+    comVTK.Update()
+    com = comVTK.GetCenter()
+
+    # Define a plane
+    plane = vtk.vtkPlane()
+    plane.SetOrigin(com)
+    plane.SetNormal(0,0,1)
+
+    # Define a plane collection (used by clipclosedsurface)
+    planeS = vtk.vtkPlaneCollection()
+    planeS.AddItem(plane)
+
+    #Setup cut - this does not close the surfaces (makes hollow)
+    clip = vtk.vtkClipPolyData()
+    clip.SetClipFunction(plane)
+    clip.SetInputData(polys)
+    clip.GenerateClippedOutputOn()
+    clip.Update()
+    clipped1 = clip.GetOutput()
+    clipped2 = clip.GetClippedOutput()
+
+    # # Cut and close the surface (this caps the whole volume !)
+    # # Alos needs to be done for each half
+    # clip = vtk.vtkClipClosedSurface()
+    # clip.SetClippingPlanes(planeS)
+    # clip.SetInputData(polys)
+    # clip.GenerateOutlineOn()
+    # clip.Update()
+    # clipped1 = clip.GetOutput()
+    # clipped2 = clip.GetOutput()
+
+    # # Using cutter (3D - 2D) to get the lines so we can fill in
+    # cut = vtk.vtkCutter()
+
+    # cut.SetCutFunction(plane)
+    # cut.SetInputData(polys) 
+    # cut.Update()
+    # cut1 = cut.GetOutput()
+
+    # strip = vtk.vtkStripper()
+    # strip.SetInputData(cut1)
+    # strip.Update()
+    # s = strip.GetOutput()
+
+    # # Use a ruled surface filer to fill in the ends
+    # rs = vtk.vtkRuledSurfaceFilter()
+    # rs.SetInputData(s)
+    # rs.CloseSurfaceOn()
+    # rs.Update()
+    # end = rs.GetOutput()
+
+    # clipped1 = end
+    # clipped2 = end
+
+    # # We need to make these solid now
+    # bcf = vtk.vtkBandedPolyDataContourFilter()
+    # bcf.SetInputData(clipped1)
+    # bcf.GenerateValues(10, -0.1, 2.0)
+    # bcf.SetScalarModeToIndex()
+    # bcf.GenerateContourEdgesOff()
+    # bcf.Update()
+    # bcf1 = bcf.GetOutput()
+
+    return clipped1, clipped2
+
+# Write the STL file
+def writeSTL(polys, filename, extension):
+
+    tmpFile = filename + extension
+    organWriter=vtk.vtkSTLWriter()
+    organWriter.SetFileName(tmpFile)
+    organWriter.SetInputData(polys)
+    organWriter.Write()
+
+    print "Writen STL Shell [", tmpFile, "]"
